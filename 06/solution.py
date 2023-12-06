83f8389c114eea48ded0da3c5a3e914b94a67748
smuuule <samuel.runmark@gmail.com>
2023-12-06 15:34:20 +0100

    Simplified 06 python

--- 06/solution.py
+++ 06/solution.py
@@ -1,10 +1,6 @@
-def part1(input):
-
-    time, distance = [j.split() for j in [i.split(": ")[1] for i in input.splitlines()]]
-    time = [int(i) for i in time]
-    distance = [int(i) for i in distance]
-
-    waysToWin = []
+def waysToWin(time, distance):
+    
+    waysToWin = []  
     race = 0
     while race < len(time):
 
@@ -16,28 +12,29 @@ def part1(input):
         waysToWin.append(len(raceDistance))
 
         race += 1
-    
+
+    return waysToWin
+
+def part1(input):
+
+    time, distance = [j.split() for j in [i.split(": ")[1] for i in input.splitlines()]]
+    time = [int(i) for i in time]
+    distance = [int(i) for i in distance]
+
     result = 1
-    for i in waysToWin:
+    for i in waysToWin(time, distance):
         result *= i
 
     return result
     
 def part2(input):
 
-    time, distance = [int(i.split(":")[1].replace(" ", "")) for i in input.splitlines()]
-    print(time, distance)
+    time, distance = [[int(i.split(":")[1].replace(" ", ""))] for i in input.splitlines()]
 
-    waysToWin = []
-    raceDistance = []
-    for i in range(0, time):
-        raceDistance.append(i * (time - i))
-
-    raceDistance = list(filter(lambda i: i > distance, raceDistance))
-    waysToWin.append(len(raceDistance))
+    waysToWin(time, distance)
     
     result = 1
-    for i in waysToWin:
+    for i in waysToWin(time, distance):
         result *= i
 
     return result
