8d479ec1876886efe8cfdc6b2376aa643588ec5d
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2023-12-08 18:00:23 +0100

    day 08 python

--- 08/solution.py
+++ 08/solution.py
@@ -1,19 +1,29 @@
+def gcd(a: int, b: int) -> int:
+    if b == 0:
+        return a
+    return gcd(b, a%b)
+
+def lcm(lst: list) -> int:
+    curr = 1
+    for i in lst:
+        curr = (curr * i) // gcd(curr, i)
+    
+    return curr  
+    
 def create_network(input):
     network = dict()
-    steps, lines = input.split("\n\n")
-    for line in lines.splitlines():
+    steps, _, *lines = input.splitlines()
+    for line in lines:
         key, value = line.split(" = ")
         value = value[1:-1].split(", ")
         network[key] = value
     return steps, network
 
-def part1(input):
-    steps, network = create_network(input)
-        
+def network_steps(network, steps, start, end):
     count = 0
     c = 0
-    curr = 'AAA'
-    while curr != 'ZZZ':
+    curr = start
+    while curr not in end:
         count += 1
         
         dir = 0 if steps[c] == 'L' else 1
@@ -24,28 +34,20 @@ def part1(input):
             c = 0
     
     return count
+
+def part1(input):
+    steps, network = create_network(input)
+        
+    return network_steps(network, steps, 'AAA', ['ZZZ'])
     
 def part2(input):
     steps, network = create_network(input)
     
     starts = [x for x in network.keys() if x[-1] == 'A']
+    ends = [x for x in network.keys() if x[-1] == 'Z']
+    step_amounts = []
     
-    record = 0
-    count = 0
-    c = 0
-    curr = starts
-    while len([x for x in curr if x[-1] == 'Z']) != len(starts):
-        test = len([x for x in curr if x[-1] == 'Z'])
-        if test > record:
-            record = test
-            print(f"Record of simultaneous end points: {record}")
-
-        dir = 0 if steps[c] == 'L' else 1
-        curr = [network.get(i)[dir] for i in curr]
-        
-        count += 1
-        c += 1
-        if c >= len(steps):
-            c = 0
+    for start in starts:
+        step_amounts.append(network_steps(network, steps, start, ends))
     
-    return count
+    return lcm(step_amounts)
